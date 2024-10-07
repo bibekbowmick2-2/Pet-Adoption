41767ac55cb8e5e30e6014459d56f5f7ab120fd2
unknown <bibektotol@gmail.com>
2024-10-07 00:55:10 -0700

    api-advance-work

--- script.js
+++ script.js
@@ -1,16 +1,49 @@
-function allitems() {
+function scrollToSection(id) {
+    const section = document.getElementById(id);
+    section.scrollIntoView({ behavior: 'smooth' });
+}
+
+
+
+
+
+
+
+function allitems(category) {
     fetch("https://openapi.programming-hero.com/api/peddy/pets")
         .then((res) => res.json())
         .then((data) => {
-            console.log(data.pets);
-            displayItems(data.pets);
+            // console.log(data.pets);
+            // displayItems(data.pets);
+            if (category) {
+                const filteredData = data.pets.filter((element) => element.category === category);
+                console.log(filteredData);
+                displayItems(filteredData);
+            } else {
+                console.log(data.pets);
+                displayItems(data.pets);
+            }
+        });
+}
+
+
+
+function allcategories() {
+    fetch("https://openapi.programming-hero.com/api/peddy/categories")
+        .then((res) => res.json())
+        .then((data) => {
+            
+            displayCategories(data.categories);
         });
 }
 
 
 
 function displayItems(data) {
+
+
     const card_container = document.getElementById("card-container");
+    card_container.innerHTML = ``;
    data.forEach(element => {
 
     const card = document.createElement("div");
@@ -18,7 +51,7 @@ function displayItems(data) {
        <div class="w-full p-2 border-2 border-gray-200 mb-2 ">
        <div class="w-full">
            <img class="w-full" src="${element.image}" alt="" srcset=""><br>
-           <p class="font-bold text-xl">Mister Tartosh</p>
+           <p class="font-bold text-xl">${element.pet_name}</p>
        </div>
        <div class="flex w-full">
            <div class=""><img src="images/Frame.png" alt=""></div>
@@ -58,4 +91,27 @@ function displayItems(data) {
    })
 }
 
-allitems();
+
+
+
+function displayCategories(data) {
+  
+    const category_types = document.getElementById("category_types");
+
+    data.forEach(element => {
+        const category = document.createElement("div");
+        category.innerHTML = `
+        <div class="rounded-xl p-3"><button onclick="allitems('${element.category}')" class="btn w-full h-full py-3 font-bold text-xl"><img
+                            src="${element.category_icon}" alt="">${element.category}</button></div>
+        
+        `
+
+        category_types.appendChild(category);
+    })
+
+}
+
+
+
+ allitems();
+allcategories();
