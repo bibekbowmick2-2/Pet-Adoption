fded8b5a946ab9575501cc63f9825ff0f7a31cb3
unknown <bibektotol@gmail.com>
2024-10-07 01:59:24 -0700

    fix-footer and api no-data

--- script.js
+++ script.js
@@ -44,51 +44,71 @@ function displayItems(data) {
 
     const card_container = document.getElementById("card-container");
     card_container.innerHTML = ``;
-   data.forEach(element => {
+    if (data.length === 0) {
+        const no_result = document.createElement("div");
+        // no_result.classList.add("flex", "flex-col", "items-center");
+        no_result.innerHTML = `
+        <div class="flex flex-col items-center">
+        <img class="w-1/2" src="images/error.webp" alt="">
+        <h1 class="text-3xl font-bold">No Information Available</h1>
+        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at 
+        its layout.</p>
+        </div>
+        `;
+        card_container.appendChild(no_result);
+    }
 
-    const card = document.createElement("div");
-       card.innerHTML = `
-       <div class="w-full p-2 border-2 border-gray-200 mb-2 ">
-       <div class="w-full">
-           <img class="w-full" src="${element.image}" alt="" srcset=""><br>
-           <p class="font-bold text-xl">${element.pet_name}</p>
-       </div>
-       <div class="flex w-full">
-           <div class=""><img src="images/Frame.png" alt=""></div>
-           <div class="">
-               <p class="text-sm">Breed: ${element.breed}</p>
-           </div>
-       </div>
-       <div class="flex w-full">
-           <div class=""><img src="images/Frame (1).png" alt=""></div>
-           <div class="">
-               <p class="text-sm">Birth: ${element.date_of_birth}</p>
-           </div>
-       </div>
-       <div class="flex w-full">
-           <div class=""><img src="images/Frame (3).png" alt=""></div>
-           <div class="">
-               <p class="text-sm">Gender: ${element.gender}</p>
-           </div>
-       </div>
-       <div class="flex w-full">
-           <div class=""><img src="images/Frame (2).png" alt=""></div>
-           <div class="">
-               <p class="text-sm">Price : ${element.price}</p>
-           </div>
-       </div><br>
-       <div class="w-full">
-           <button class="btn"><i class="fa-regular fa-thumbs-up"></i></button>
-           <button class="btn">Adopt</button>
-           <button class="btn">Details</button>
-       </div>
 
-   </div>
+    else{
 
-       `
+        data.forEach(element => {
 
-       card_container.appendChild(card);
-   })
+            const card = document.createElement("div");
+               card.innerHTML = `
+               <div class="w-full p-2 border-2 border-gray-200 mb-2 ">
+               <div class="w-full">
+                   <img class="w-full" src="${element.image}" alt="" srcset=""><br>
+                   <p class="font-bold text-xl">${element.pet_name}</p>
+               </div>
+               <div class="flex w-full">
+                   <div class=""><img src="images/Frame.png" alt=""></div>
+                   <div class="">
+                       <p class="text-sm">Breed: ${element.breed}</p>
+                   </div>
+               </div>
+               <div class="flex w-full">
+                   <div class=""><img src="images/Frame (1).png" alt=""></div>
+                   <div class="">
+                       <p class="text-sm">Birth: ${element.date_of_birth}</p>
+                   </div>
+               </div>
+               <div class="flex w-full">
+                   <div class=""><img src="images/Frame (3).png" alt=""></div>
+                   <div class="">
+                       <p class="text-sm">Gender: ${element.gender}</p>
+                   </div>
+               </div>
+               <div class="flex w-full">
+                   <div class=""><img src="images/Frame (2).png" alt=""></div>
+                   <div class="">
+                       <p class="text-sm">Price : ${element.price}</p>
+                   </div>
+               </div><br>
+               <div class="w-full">
+                   <button class="btn"><i class="fa-regular fa-thumbs-up"></i></button>
+                   <button class="btn">Adopt</button>
+                   <button class="btn">Details</button>
+               </div>
+        
+           </div>
+        
+               `
+        
+               card_container.appendChild(card);
+           })
+
+    }
+   
 }
 
 
